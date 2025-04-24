c622723855fec3821075d3154fd2f078eb5a1d24
MP1-2 <phuongminhdo.hp@gmail.com>
2025-04-24 17:40:20 +0700

    Update ContactPage.tsx

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -4,6 +4,7 @@ import Layout from '@/components/layout/Layout';
 import { useLanguage } from '@/context/LanguageContext';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import BackButton from "@/components/common/BackButton";
 import { Textarea } from '@/components/ui/textarea';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Label } from '@/components/ui/label';
